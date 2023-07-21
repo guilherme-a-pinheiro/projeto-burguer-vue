95668f097fe114bf3cbf9ad553a96436a3580c97
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2023-07-20 21:14:57 -0300

    recebendo os status dos pedidos

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -23,7 +23,8 @@
                 </div>
                 <div>
                     <select name="status" class="status">
-                        <option value="">Selecione</option>
+                        <option value="selecione">Selecione</option>
+                        <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo">{{ s.tipo }}</option>
                     </select>
                     <button class="delete-btn">Cancelar</button>
                 </div>
@@ -70,7 +71,20 @@ export default {
             this.burgers = data
 
             console.log(this.burgers)
-        }
+
+            this.getStatus();
+
+        },
+        async getStatus() {
+
+            const req = await fetch(" http://localhost:3000/status")
+
+            const data = await req.json()
+
+            this.status = data
+
+
+        },
     },
     mounted() {
         this.getPedidos()
